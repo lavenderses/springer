aaaa39f3b8d1a8b0daa5723b8aa27f01f6cb4640
nakanoi <nakanoinryota@gmail.com>
2022-03-05 15:54:45 +0900

    Add rest configurations.

---
+++ rest/src/main/java/com/nakanoi/springer/rest/config/RestConfig.java
@@ -0,0 +1,11 @@
+package com.nakanoi.springer.rest.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/** Configuration for rest dispatcher servlet. */
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.nakanoi.springer.rest.controllers")
+public class RestConfig {}
